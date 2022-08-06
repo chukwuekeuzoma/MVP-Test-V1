b655e767ecf4b0314f47f53d445258d7ea37d12b
mac <chukwuekeuzoma@gmail.com>
2022-08-06 23:37:33 +0100

    upadated new component

---
+++ src/Pages/index.js
@@ -0,0 +1,9 @@
+import AllProjectsAllGateWays from "./AllProjectsAllGateWays/AllProjectsAllGateWays";
+import Dashboard from "./DashBoard/DashBoard"
+import NoReports from "./NoReports/NoReports";
+
+export {
+    AllProjectsAllGateWays,
+    Dashboard,
+    NoReports
+}
