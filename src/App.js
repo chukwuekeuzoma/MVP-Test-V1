22f293aac1282985677488e9a58265b76fef966d
mac <chukwuekeuzoma@gmail.com>
2022-08-06 23:35:41 +0100

    upadted the component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import HomeLayout from "./Layouts/HomeLayout";
+import {AllProjectsAllGateWays} from "./Pages"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+function RouteWithLayout({ Layout, Component, ...rest }) {
+  return (
+    <Layout {...rest}>
+      <Component {...rest} />
+    </Layout>
+  );
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+       <Routes>
+       <Route
+            path="/"
+            element={
+              <RouteWithLayout Layout={HomeLayout} Component={AllProjectsAllGateWays} />
+            }
+          />
+       </Routes>
+    </BrowserRouter>
   );
 }
 
