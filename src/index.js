3f079631ec0fd7a9d53c68a05c0ecdb9a69e0acc
mac <chukwuekeuzoma@gmail.com>
2022-08-09 13:57:22 +0100

    upadted the component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import reportWebVitals from "./reportWebVitals";
+import axios from "axios";
+import ContextProvider from "./Context/Context";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const URL = process.env.REACT_APP_API_BASE_URL;
+axios.defaults.baseURL = URL;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+      <App />
+    </ContextProvider>
   </React.StrictMode>
 );
 
