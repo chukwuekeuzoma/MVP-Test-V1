1af2d0975370b0b7cdd42f4961e4f2ea4cc973be
mac <chukwuekeuzoma@gmail.com>
2022-08-09 13:57:07 +0100

    upadted the component

--- src/Pages/DashBoard/DashBoard.jsx
+++ src/Pages/DashBoard/DashBoard.jsx
@@ -1,31 +1,71 @@
-import React from "react";
-import Select from "react-select";
+import React, { useState, useEffect, useContext } from "react";
+import { Context } from "../../Context/Context";
+import axios from "axios";
 import "./DashBoard.css";
+
 function DashBoard(props) {
+  const { useProjectValue,useGetWayProjectValue } = useContext(Context);
+
+  const [project, setProject] = useState([]);
+  const [projectValue, setProjectValue] = useState("");
+  const [gateWayValue, setGateWayValue] = useState("");
+  const [loaderCheck, setLoaderCheck] = useState(false);
+  const [gateWays, setGetWays] = useState([]);
+
+  const getProjects = async () => {
+    let response = await axios.get("projects");
+    setProject(response.data.data);
+  };
+
+  const getGateWays = async () => {
+    let response = await axios.get("gateways");
+    setGetWays(response.data.data);
+  };
+
+  useEffect(() => {
+    getProjects();
+    getGateWays();
+  }, []);
+
+  const collectValue = () => {
+    useProjectValue(projectValue);
+    useGetWayProjectValue(gateWayValue)
+  };
+
+  // console.log();
+
   return (
     <div className="dashboard-container">
       <div>
         <div className="reports">Reports</div>
-        <div className="easily">Easily generate a report of your transactions</div>
+        <div className="easily">
+          Easily generate a report of your transactions
+        </div>
       </div>
       <div className="dashboard-inner">
         <div className="custom-select d-margin-right">
-          <select>
-            <option value="">Select type</option>
-            <option value="">Martins</option>
-            <option value="">Uzoma</option>
-            <option value="">Oliver</option>
+          <select onChange={e => setProjectValue(e.target.value)}>
+            <option value="">Select project</option>
+            <option value="All Project">All Project</option>
+            {project.map(({ name }, index) => (
+              <option value={name} key={index}>
+                {name}
+              </option>
+            ))}
           </select>
           <span className="custom-arrow"></span>
         </div>
         <div className="custom-select d-margin-right">
-          <select>
-            <option value="">Select type</option>
-            <option value="">Martins</option>
-            <option value="">Uzoma</option>
-            <option value="">Oliver</option>
+          <select onChange={ e => setGateWayValue(e.target.value)}>
+            <option value="">Select GateWay</option>
+            <option value="All Gateways">All Gateways</option>
+            {gateWays.map(({ name }, index) => (
+              <option value={name} key={index}>
+                {name}
+              </option>
+            ))}
           </select>
-          <span className="custom-arrow"></span>
+          <span className="custom-arrow-two"></span>
         </div>
         <div className="d-margin-right">
           <label htmlFor="date" className="date-form">
@@ -39,7 +79,9 @@ function DashBoard(props) {
           </label>
           <input type="date" />
         </div>
-        <div className="generate-report d-margin-right"><div>Generate report</div></div>
+        <div className="generate-report d-margin-right" onClick={collectValue}>
+          <div>Generate report</div>
+        </div>
       </div>
     </div>
   );
