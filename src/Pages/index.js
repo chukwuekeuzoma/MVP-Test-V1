189c527aaac589f5a91bce26b1cb3706cfb29339
mac <chukwuekeuzoma@gmail.com>
2022-08-13 00:52:09 +0100

    updated the index.js file

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,9 +1,11 @@
 import AllProjectsAllGateWays from "./AllProjectsAllGateWays/AllProjectsAllGateWays";
+import AllProjectOneGateWay from "./AllProjectOneGateWay/AllProjectOneGateWay";
 import Dashboard from "./DashBoard/DashBoard"
 import NoReports from "./NoReports/NoReports";
 
 export {
     AllProjectsAllGateWays,
     Dashboard,
-    NoReports
+    NoReports,
+    AllProjectOneGateWay
 }
