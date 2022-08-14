86b57054e51e716da6b433341e416fdcc9e6db1e
mac <chukwuekeuzoma@gmail.com>
2022-08-14 01:59:28 +0100

    updated the header

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -4,21 +4,42 @@ export const Context = createContext();
 
 export default function ContextProvider(props) {
   const [projectValueGlobal, setprojectValueGlobal] = useState("");
-  const [GateWayValueGlobal, setgatewayValueGlobal] = useState("")
+  const [GateWayValueGlobal, setgatewayValueGlobal] = useState("");
+  const [firstNameSplit, setFirstNameSplit] = useState([]);
+  const [lastNameSplit, setLastNameSplit] = useState([]);
+  const [userGlobal, setUserGlobal] = useState([]);
 
   function useProjectValue(props) {
     setprojectValueGlobal(props);
   }
 
   function useGetWayProjectValue(props) {
-    setgatewayValueGlobal(props)
+    setgatewayValueGlobal(props);
+  }
+
+  function User(props) {
+    setUserGlobal(props);
+  }
+
+  function FNameSplit(props) {
+    setFirstNameSplit(props)
+  }
+
+  function LNameSplit (props) {
+    setLastNameSplit(props)
   }
 
   const value = {
     projectValueGlobal,
     GateWayValueGlobal,
+    userGlobal,
+    firstNameSplit,
+    lastNameSplit,
     useProjectValue,
     useGetWayProjectValue,
+    User,
+    FNameSplit,
+    LNameSplit
   };
 
   return <Context.Provider value={value}>{props.children}</Context.Provider>;
