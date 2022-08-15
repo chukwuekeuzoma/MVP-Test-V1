80f2e3ce9f294001094fb34a3a37d4338ecacf88
mac <chukwuekeuzoma@gmail.com>
2022-08-15 12:44:05 +0100

    updated the component

--- src/Pages/AllProjectOneGateWay/AllProjectOneGateWay.jsx
+++ src/Pages/AllProjectOneGateWay/AllProjectOneGateWay.jsx
@@ -1,14 +1,16 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useContext } from "react";
 import { Context } from "../../Context/Context";
 import "./AllProjectOneGateWay.css";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
+import moment from "moment";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 function AllProjectOneGateWay(props) {
+  const { project1AllProjectsGateWay1 } = useContext(Context);
+
   const [openProject, setOpenProject] = useState("");
-  const [viewProject, setViewProject] = useState("NoReport");
 
   const projectOneOpen = () => {
     setOpenProject("project1");
@@ -18,19 +20,30 @@ function AllProjectOneGateWay(props) {
     setOpenProject("project2");
   };
 
+  console.log(project1AllProjectsGateWay1);
+
+  //sum of project1AllProjectsGateWay1 
+  const sumTotalOfproject1AllProjectsGateWay1 = project1AllProjectsGateWay1.reduce(
+    (accumulator, currentValue) => {
+      return accumulator + currentValue.amount;
+    },
+    0
+  );
+
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 0
+  });
+
   const data = {
-    labels: ["Project 1", "Project 2", "Project 3", "Project 4"],
+    labels: ["Project 1", "Project 2"],
     datasets: [
       {
         label: "# of Votes",
-        data: [12, 19, 3, 5],
+        data: [12, 19],
         backgroundColor: ["#A259FF", "#F24E1E", "#FFC107", " #6497B1"],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)"
-        ],
+        borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)"],
         borderWidth: 0
       }
     ]
@@ -55,7 +68,11 @@ function AllProjectOneGateWay(props) {
           <div className="allprojectonegateway">All projects | Gateway 1</div>
           <div className="project-container" onClick={projectOneOpen}>
             <div>Project 1</div>
-            <div>TOTAL: 10,065 USD</div>
+            <div>
+              {" "}
+              TOTAL:&nbsp;
+              {formatter.format(sumTotalOfproject1AllProjectsGateWay1)}
+            </div>
           </div>
           <div
             className={
@@ -68,30 +85,18 @@ function AllProjectOneGateWay(props) {
               <thead>
                 <tr>
                   <th>Date</th>
-                  <th>Gateway</th>
                   <th>Transaction ID</th>
                   <th>Amount</th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>01/21/2021</td>
-                  <td>Gateway 2</td>
-                  <td>a732b</td>
-                  <td>3964 USD</td>
+                {project1AllProjectsGateWay1.map(({ amount, created, paymentId }, index) => (
+                <tr key={index}>
+                  <td>{moment(created).format("MM-DD-YYYY")}</td>
+                  <td>{paymentId}</td>
+                  <td>{formatter.format(amount)}</td>
                 </tr>
-                <tr>
-                  <td>01/21/2021</td>
-                  <td>Gateway 2</td>
-                  <td>a732b</td>
-                  <td>3964 USD</td>
-                </tr>
-                <tr>
-                  <td>01/21/2021</td>
-                  <td>Gateway 2</td>
-                  <td>a732b</td>
-                  <td>3964 USD</td>
-                </tr>
+                ))}
               </tbody>
             </table>
           </div>
