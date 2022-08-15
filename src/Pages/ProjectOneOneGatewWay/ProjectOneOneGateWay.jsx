0470c4acd5f76b08b5d442622e57bb4411171a68
mac <chukwuekeuzoma@gmail.com>
2022-08-15 12:44:20 +0100

    updated the component

--- src/Pages/ProjectOneOneGatewWay/ProjectOneOneGateWay.jsx
+++ src/Pages/ProjectOneOneGatewWay/ProjectOneOneGateWay.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "../../Context/Context";
+import moment from "moment";
 import "./ProjectOneOneGateWay.css";
 
 function ProjectOneOneGateWay(props) {
+  const { reportsForPRojectOne } = useContext(Context);
+
+  //sum of Project1
+  const sumTotalUnProjectOneAmount = reportsForPRojectOne.reduce(
+    (accumulator, currentValue) => {
+      return accumulator + currentValue.amount;
+    },
+    0
+  );
+
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 0
+  });
+
   return (
     <>
       <div className="projectOneOneGateway-container">
@@ -10,35 +28,28 @@ function ProjectOneOneGateWay(props) {
           <thead>
             <tr>
               <th>Date</th>
-              <th>Gateway</th>
               <th>Transaction ID</th>
               <th>Amount</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>01/21/2021</td>
-              <td>Gateway 2</td>
-              <td>a732b</td>
-              <td>3964 USD</td>
-            </tr>
-            <tr>
-              <td>01/21/2021</td>
-              <td>Gateway 2</td>
-              <td>a732b</td>
-              <td>3964 USD</td>
-            </tr>
-            <tr>
-              <td>01/21/2021</td>
-              <td>Gateway 2</td>
-              <td>a732b</td>
-              <td>3964 USD</td>
-            </tr>
+            {reportsForPRojectOne.map(
+              ({ amount, created, paymentId }, index) => (
+                <tr key={index}>
+                  <td>{moment(created).format("MM-DD-YYYY")}</td>
+                  <td>{paymentId}</td>
+                  <td>{formatter.format(amount)}</td>
+                </tr>
+              )
+            )}
           </tbody>
         </table>
       </div>
       <div className="projectOneOneGateway-container">
-        <div className="total">TOTAL: 10,065 USD</div>
+        <div className="total">
+          TOTAL:&nbsp;
+          {formatter.format(sumTotalUnProjectOneAmount)}
+        </div>
       </div>
     </>
   );
